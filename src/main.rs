22444327bf09898825b3bf3c7d987aeb9fed34fd
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-07-29 16:38:41 +0000

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,31 @@
 use std::io::{self, Write};
 
 fn main() {
+    let command :Vec<&str> = vec!["echo", "cat", "exit", "type"];
     // Uncomment this block to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
-
     // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
     while stdin.read_line(&mut input).is_ok() {
-        if input.contains("exit") {
+        if input.starts_with("exit") {
             return;
-        }else if input.contains("echo"){
+        }else if input.starts_with("echo"){
             let echo_input = input.trim_start_matches("echo ");
             print!("{}", echo_input);
-            print!("$ ");
-            input.clear();
-            io::stdout().flush().unwrap();
+        }else if input.starts_with("type"){
+            let table_input: Vec<&str> = input.trim().split(" ").collect();
+            if command.contains(&table_input[1]){
+                println!("{} is a shell builtin", &table_input[1]);
+            }else{
+                println!("{}: not found", &table_input[1]);
+            }
         }else{
             println!("{}: command not found", input.trim());
-            print!("$ ");
-            input.clear();
-            io::stdout().flush().unwrap();
         }
+        print!("$ ");
+        input.clear();
+        io::stdout().flush().unwrap();
     };
 }
