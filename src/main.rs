0e7bd2350329e27a250abccba488deba31c5f9a4
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-07-29 13:39:59 +0000

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,8 @@ fn main() {
     // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    
-    while input.contains("invalid"){
+
+    while stdin.read_line(&mut input).is_ok() {
         println!("{}: command not found", input.trim());
         print!("$ ");
         input.clear();
